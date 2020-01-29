2d9fd7dd12c30a4a5aaa469b44b2525692be0232
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-29 06:13:44 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,27 +1,30 @@
 def new_hash
-
+powers = {}
 end
 
 def actor
-
+actor = (
+:name=> ["Brit Marling"]
+)
 end
 
-def monopoly
-	monopoly = {}
-
-end
-
-def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
-end
-
-def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
-end
-
-def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
-end
+def monopoly = { 
+  :railroads=>
+{
+  :pieces=>4
+:rent_in_dollars=>
+    {
+:one_piece_owned=>25, 
+:two_pieces_owned=> 50,
+:three_pieces_owned=>100
+:four_pieces_owned=>200
+    },
+    :names=> { 
+:reading_railroads=> {"mortage_value"=>"$100"},
+  :pennslyvania_railroad=> {"mortgage_value"=>"$200"}, :b_and_o_railraods=> {"mortgage_value"=>"$400"},
+     :shortline=> {"mortgage_value"=>"$800"}
+    }
+  
+    
+    }
+    }
